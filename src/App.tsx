df325c4e43fc94acac65a3d4ec1b213cae527cc1
CarolineS02 <southardsmithc@gmail.com>
2024-09-10 20:24:34 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <h1> Welcome to web dev!</h1>
+      <h2>Lab 1</h2>
+      <h3>Lab 2</h3>
+      {/* export default function Labs() { return <div> ... </div>} 
+      ^this can be put in a seperate file and called with an import in this file so they can all be included without the noise of everything else*/}
+      <h4>Lab 3</h4>
+
+    </div>
+  );
+}
+
+export default App;
