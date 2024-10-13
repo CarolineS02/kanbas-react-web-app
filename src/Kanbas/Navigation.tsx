7f68c731e66c7a26c55d12d4c367cb05c8604d3f
CarolineS02 <southardsmithc@gmail.com>
2024-10-13 15:14:19 -0400

    Updated nav bar

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -9,32 +9,32 @@ function App() {
   const { pathname } = useLocation();
   const links = [
     { label: "Dashboard", path: "/Kanbas/Dashboard", icon: AiOutlineDashboard },
-    { label: "Courses",   path: "/Kanbas/Dashboard", icon: LiaBookSolid },
-    { label: "Calendar",  path: "/Kanbas/Calendar",  icon: IoCalendarOutline },
-    { label: "Inbox",     path: "/Kanbas/Inbox",     icon: FaInbox },
-    { label: "Labs",      path: "/Labs",             icon: LiaCogSolid },
+    { label: "Courses", path: "/Kanbas/Dashboard", icon: LiaBookSolid },
+    { label: "Calendar", path: "/Kanbas/Calendar", icon: IoCalendarOutline },
+    { label: "Inbox", path: "/Kanbas/Inbox", icon: FaInbox },
+    { label: "Labs", path: "/Labs", icon: LiaCogSolid },
   ];
   return (
     <div className="nav-bar">
       <div id="wd-kanbas-navigation" style={{ width: 105 }} className="list-group rounded-0 position-fixed bottom-0 top-0 bg-black z-2 d-flex flex-column align-items-center">
-      <a id="wd-neu-link" target="_blank" 
-        href="https://www.northeastern.edu/"
-        className="list-group-item bg-black border-0 text-center w-100">
-        <img src="/images/NEU.png" width="75px" /></a>
-        <Link to="/Kanbas/Account" className={`list-group-item text-center border-0 bg-black
-            ${pathname.includes("Account") ? "bg-white text-danger" : "bg-black text-white"}`}>
-        <FaRegCircleUser className={`fs-1 ${pathname.includes("Account") ? "text-danger" : "text-white"}`} />
-        <br />
-        Account
-      </Link>
-      {links.map((link) => (
-        <Link key={link.path} to={link.path} className={`list-group-item bg-black text-center border-0
-              ${pathname.includes(link.label) ? "text-danger bg-white" : "text-white bg-black"}`}>
-          {link.icon({ className: "fs-1 text-danger"})}
-          <br />
-          {link.label}
+        <a id="wd-neu-link" target="_blank"
+          href="https://www.northeastern.edu/"
+          className="list-group-item bg-black border-0 text-center w-100">
+          <img src="/images/NEU.png" width="75px" /></a>
+        <Link to="/Kanbas/Account" id="wd-account-link"
+          className={`list-group-item text-center border-0 w-100 
+          ${pathname.includes("Account") ? 'bg-white text-danger' : 'bg-black text-white'}`}>
+          <FaRegCircleUser className="fs-4" /><br />
+          <span>Account</span>
         </Link>
-      ))}
+        {links.map((link) => (
+          <Link to={link.path} key={link.path} id="wd-account-link"
+            className={`list-group-item text-center border-0 w-100 
+          ${pathname.includes(link.label) ? 'bg-white text-danger' : 'bg-black text-white'}`}>
+            {link.icon({ className: "fs-4 text-danger" })}<br />
+            <span>{link.label}</span>
+          </Link>
+        ))}
       </div>
     </div>
   );
