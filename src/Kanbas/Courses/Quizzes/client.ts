4aa1dff84940f9a1ab07f6bb1491b1101542b78b
CarolineS02 <southardsmithc@gmail.com>
2024-11-15 20:26:07 -0500

    test deploy for netlify 2

--- src/Kanbas/Courses/Quizzes/client.ts
+++ src/Kanbas/Courses/Quizzes/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const QUIZZES_API = `${REMOTE_SERVER}/api/quizzes`;
-
+// this comment is a test for a github /netify deploy :) ignore it 
 export const updateQuiz = async (quiz: any) => {
     const { data } = await axios.put(`${QUIZZES_API}/${quiz._id}`, quiz);
     return data;
